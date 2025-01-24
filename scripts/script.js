65b1417a5b7848170e7b855a0bbf4c50a4aa12c5
Kyle Mwendwa <kklegendofgaming@gmail.com>
2025-01-24 14:57:17 +0300

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+let akanNames = {
+  male: ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"],
+  female: ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"],
+};
+
+function calculateIndex() {
+  const form = (document.getElementsByTagName("gender");
+  const selectedGender = form.querySelector('input[name="gender"]:checked');
+
+  const output = document.getElementById("output");
+
+}
